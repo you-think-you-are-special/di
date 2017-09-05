32597dd484cb2c53708e9f5bf3f242aa65083d75
Litvinov Alexander <alexander.litvinov@onetwotrip.com>
2017-09-05 12:28:34 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='di',
+      version='1.0',
+      description='Dependency injection container',
+      author='Litvinov Alexander',
+      author_email='sanderlimail@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['di'],
+     )
